eeb291f29ea16561dae89e5f25299ffc8bcd03c6
abhilash soma <abhilashsoma@abhilashs-MacBook-Pro.local>
2020-02-26 22:02:01 -0600

    Updates

---
+++ precache-manifest.3b0ea9525a083aceb7f719f8b455bd93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc8b3d089b2945f2bcba38c91cc5b6b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "92d1260be6a667b42be4",
+    "url": "/monsters-rolodex/static/css/main.22de6845.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d1260be6a667b42be4",
+    "url": "/monsters-rolodex/static/js/main.44923eaf.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
